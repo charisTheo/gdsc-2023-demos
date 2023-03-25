93019cef911427b27d59e71fac86127712a274ff
charisTheo <me@harrytheo.com>
2023-03-25 13:18:50 +0000

    Added disclosure widget examples. Added smooth scrolling and reduced motion media query. Fixed backdrop z index.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+const nav = document.querySelector('nav');
 const menuOpenButton = document.querySelector('#menu-open-button');
-menuOpenButton.addEventListener('click', () => {
-  document.querySelector('nav').classList.add('open');
-});
-
 const menuCloseButton = document.querySelector('#menu-close-button');
-menuCloseButton.addEventListener('click', () => {
-  document.querySelector('nav').classList.remove('open');
-});
 
+menuOpenButton.addEventListener('click', openMenu);
+nav.addEventListener('blur', closeMenu);
+menuCloseButton.addEventListener('click', closeMenu);
+
+function openMenu() {
+  nav.classList.add('open');
+  nav.focus();
+}
+function closeMenu() {
+  // Allow the menu to close after the link click event has finished bubbling
+  setTimeout(() => {
+    nav.classList.remove('open');
+  }, 100);
+}
 
 const themeToggleButton = document.querySelector('#theme-toggle-button');
 const initialThemePreference = window.matchMedia('(prefers-color-scheme: dark)').matches;
